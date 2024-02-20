5a2a13bf67c802e202e715935a9590f5b1fbe731
reikrom <reikrom.lv@gmail.com>
2024-02-20 22:02:34 +0200

    run two servers at the same time

--- main.go
+++ main.go
@@ -1,33 +1,77 @@
 package main
 
 import (
-    "errors"
-    "fmt"
-    "io"
-    "net/http"
-    "os"
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"net"
+	"net/http"
 )
 
-func getRoot (w http.ResponseWriter, r *http.Request) {
-    fmt.Printf("got / request\n")
-    io.WriteString(w, "This is my website!\n")
+type contextKey string
+
+const keyServerAddr contextKey = "serverAddr"
+
+func getRoot(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	fmt.Printf("%s: got / request\n", ctx.Value(keyServerAddr))
 }
-func getHello (w http.ResponseWriter, r *http.Request) {
-    fmt.Printf("got /hello request\n")
-    io.WriteString(w, "Hello, HTTP!\n")
+func getHello(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	fmt.Printf("%s: got /hello request\n", ctx.Value(keyServerAddr))
+    _, err := io.WriteString(w, "Hello, HTTP!\n")
+    if err != nil {
+        fmt.Printf("Error writing response: %s\n", err)
+        http.Error(w, "internal server error", http.StatusInternalServerError)
+        return
+    }
 }
 
 func main() {
-    fmt.Println("Starting server")
-    http.HandleFunc("/", getRoot)
-    http.HandleFunc("/hello", getHello)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", getRoot)
+	mux.HandleFunc("/hello", getHello)
 
-    err := http.ListenAndServe(":3333", nil)
-    if errors.Is(err, http.ErrServerClosed) {
-        fmt.Printf("server closed\n")
-    } else if err != nil {
-        fmt.Printf("error starting server: %s\n", err)
-        os.Exit(1)
-        }
-    }
+	ctx, cancelCtx := context.WithCancel(context.Background())
+	serverOne := &http.Server{
+		Addr:    ":3333",
+		Handler: mux,
+		BaseContext: func(l net.Listener) context.Context {
+			ctx = context.WithValue(ctx, keyServerAddr, l.Addr().String())
+			return ctx
+		},
+	}
+	serverTwo := &http.Server{
+		Addr:    ":4444",
+		Handler: mux,
+		BaseContext: func(l net.Listener) context.Context {
+			ctx = context.WithValue(ctx, keyServerAddr, l.Addr().String())
+			return ctx
+		},
+	}
 
+	go func() {
+		err := serverOne.ListenAndServe()
+		if errors.Is(err, http.ErrServerClosed) {
+			fmt.Printf("server one closed \n")
+		} else if err != nil {
+			fmt.Printf("error listening for server one  %s\n", err)
+		}
+		cancelCtx()
+	}()
+	go func() {
+		err := serverTwo.ListenAndServe()
+		if errors.Is(err, http.ErrServerClosed) {
+			fmt.Printf("server two closed \n")
+		} else if err != nil {
+			fmt.Printf("error listening for server two  %s\n", err)
+		}
+		cancelCtx()
+	}()
+	fmt.Println("Starting server")
+	<-ctx.Done()
+
+}
