586ba7066ebce767ed648271d1e9a9f29c5e69cd
reikrom <reikrom.lv@gmail.com>
2024-02-20 22:21:22 +0200

    query-params

--- main.go
+++ main.go
@@ -16,18 +16,31 @@ const keyServerAddr contextKey = "serverAddr"
 func getRoot(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 
-	fmt.Printf("%s: got / request\n", ctx.Value(keyServerAddr))
+	hasFirst := r.URL.Query().Has("first")
+	first := r.URL.Query().Get("first")
+	hasSecond := r.URL.Query().Has("second")
+	second := r.URL.Query().Get("second")
+
+	fmt.Printf("%s: got / request first(%t)=%s, second(%t)=%s\n", ctx.Value(keyServerAddr),
+		hasFirst, first,
+		hasSecond, second)
+	_, err := io.WriteString(w, "This is my website!\n")
+	if err != nil {
+		fmt.Printf("Error writing response: %s\n", err)
+		http.Error(w, "internal server error", http.StatusInternalServerError)
+		return
+	}
 }
 func getHello(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 
 	fmt.Printf("%s: got /hello request\n", ctx.Value(keyServerAddr))
-    _, err := io.WriteString(w, "Hello, HTTP!\n")
-    if err != nil {
-        fmt.Printf("Error writing response: %s\n", err)
-        http.Error(w, "internal server error", http.StatusInternalServerError)
-        return
-    }
+	_, err := io.WriteString(w, "Hello, HTTP!\n")
+	if err != nil {
+		fmt.Printf("Error writing response: %s\n", err)
+		http.Error(w, "internal server error", http.StatusInternalServerError)
+		return
+	}
 }
 
 func main() {
@@ -35,8 +48,8 @@ func main() {
 	mux.HandleFunc("/", getRoot)
 	mux.HandleFunc("/hello", getHello)
 
-	ctx, cancelCtx := context.WithCancel(context.Background())
-	serverOne := &http.Server{
+	ctx  := context.Background()
+	server := &http.Server{
 		Addr:    ":3333",
 		Handler: mux,
 		BaseContext: func(l net.Listener) context.Context {
@@ -44,34 +57,14 @@ func main() {
 			return ctx
 		},
 	}
-	serverTwo := &http.Server{
-		Addr:    ":4444",
-		Handler: mux,
-		BaseContext: func(l net.Listener) context.Context {
-			ctx = context.WithValue(ctx, keyServerAddr, l.Addr().String())
-			return ctx
-		},
-	}
 
-	go func() {
-		err := serverOne.ListenAndServe()
+		err := server.ListenAndServe()
 		if errors.Is(err, http.ErrServerClosed) {
 			fmt.Printf("server one closed \n")
 		} else if err != nil {
 			fmt.Printf("error listening for server one  %s\n", err)
 		}
-		cancelCtx()
-	}()
-	go func() {
-		err := serverTwo.ListenAndServe()
-		if errors.Is(err, http.ErrServerClosed) {
-			fmt.Printf("server two closed \n")
-		} else if err != nil {
-			fmt.Printf("error listening for server two  %s\n", err)
-		}
-		cancelCtx()
-	}()
+
 	fmt.Println("Starting server")
-	<-ctx.Done()
 
 }
