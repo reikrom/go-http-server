bad2002080a82d8b89404f8f897734ca93baf72c
reikrom <reikrom.lv@gmail.com>
2024-02-21 08:03:32 +0200

    bad post request

--- main.go
+++ main.go
@@ -21,9 +21,11 @@ func getRoot(w http.ResponseWriter, r *http.Request) {
 	hasSecond := r.URL.Query().Has("second")
 	second := r.URL.Query().Get("second")
 
-	fmt.Printf("%s: got / request first(%t)=%s, second(%t)=%s\n", ctx.Value(keyServerAddr),
+	fmt.Printf("%s: got / request first(%t)=%s, second(%t)=%s\n",
+		ctx.Value(keyServerAddr),
 		hasFirst, first,
 		hasSecond, second)
+
 	_, err := io.WriteString(w, "This is my website!\n")
 	if err != nil {
 		fmt.Printf("Error writing response: %s\n", err)
@@ -35,7 +37,16 @@ func getHello(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 
 	fmt.Printf("%s: got /hello request\n", ctx.Value(keyServerAddr))
-	_, err := io.WriteString(w, "Hello, HTTP!\n")
+
+	myName := r.PostFormValue("myName")
+	if myName == "" {
+		// myName = "HTTP"
+		w.Header().Set("x-missing-field", "myName")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	_, err := io.WriteString(w, fmt.Sprintf("Hello, %s!\n", myName))
 	if err != nil {
 		fmt.Printf("Error writing response: %s\n", err)
 		http.Error(w, "internal server error", http.StatusInternalServerError)
@@ -48,7 +59,7 @@ func main() {
 	mux.HandleFunc("/", getRoot)
 	mux.HandleFunc("/hello", getHello)
 
-	ctx  := context.Background()
+	ctx := context.Background()
 	server := &http.Server{
 		Addr:    ":3333",
 		Handler: mux,
@@ -58,12 +69,12 @@ func main() {
 		},
 	}
 
-		err := server.ListenAndServe()
-		if errors.Is(err, http.ErrServerClosed) {
-			fmt.Printf("server one closed \n")
-		} else if err != nil {
-			fmt.Printf("error listening for server one  %s\n", err)
-		}
+	err := server.ListenAndServe()
+	if errors.Is(err, http.ErrServerClosed) {
+		fmt.Printf("server one closed \n")
+	} else if err != nil {
+		fmt.Printf("error listening for server one  %s\n", err)
+	}
 
 	fmt.Println("Starting server")
 
